3c3d158edfcf5cbc3b7af34818df0d305038e2ed
amitp <amitpatel_7@msn.com>
2018-10-19 22:03:26 -0700

    created Calculator component and added basic styles. Rendered it in parent App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Calculator from './Calculator.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Calculator></Calculator>
       </div>
     );
   }
